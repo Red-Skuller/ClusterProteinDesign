d155dd94d0fd3c5d6b5611558063c6d4c5d777db
Red-Skuller <lh7128@student.uni-lj.si>
2024-04-18 10:00:24 +0200

    added echo rfdiff config to slurm.out

--- pipelines/binder_design/helper_scripts/binder_dock_nothread.sh
+++ pipelines/binder_design/helper_scripts/binder_dock_nothread.sh
@@ -98,7 +98,7 @@ do
       rm -r "$output/rf_dock/${prefix}_${subfolder_name}_${SLURM_ARRAY_TASK_ID}/traj" # delete trajectories
   fi
   echo "$hotspots" > "$output/hotspots.txt"
-  
+  echo "$rfdiff_config_string" > "$output/rfdiff_config.txt"
   ########################
   # ProteinMPNN and AF2
   ########################
